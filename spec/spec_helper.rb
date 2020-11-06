02e143d682135ce138738db03ade4d9173f474b8
Sam Livingston-Gray <geeksam@gmail.com>
2020-11-06 10:55:42 -0800

    yoink spec_helper from check_please

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,18 @@
 require "bundler/setup"
 require "check_please_rspec_matcher"
 
+
+
+require 'pathname'
+PROJECT_ROOT = Pathname.new(
+  File.expand_path(
+    File.join( File.dirname(__FILE__), '..' )
+  )
+)
+Dir[ PROJECT_ROOT.join('spec', 'support', '**', '*.rb') ].each { |f| require f }
+
+
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
@@ -10,5 +22,30 @@ RSpec.configure do |config|
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+    c.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |c|
+    c.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.filter_run_when_matching :focus
+  config.example_status_persistence_file_path = "spec/examples.txt"
+  config.disable_monkey_patching!
+  if config.files_to_run.one?
+    config.default_formatter = "doc"
+  end
+  config.order = :random
+  Kernel.srand config.seed
+end
+
+
+
+# Always clear the global that Sam has a tendency to set
+RSpec.configure do |config|
+  config.after(:each) do
+    $debug = false
   end
 end
+
