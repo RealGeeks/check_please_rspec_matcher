b290fce29f362d5b60388e657162126a84297a03
Sam Livingston-Gray <geeksam@gmail.com>
2021-02-05 11:23:07 -0800

    Bump version to 0.5.2, depend on check_please 0.5.x

--- lib/check_please_rspec_matcher/version.rb
+++ lib/check_please_rspec_matcher/version.rb
@@ -1,4 +1,4 @@
 module CheckPleaseRspecMatcher
   # NOTE: don't forget to change the dependency on the 'check_please' gem in the .gemspec file
-  VERSION = "0.5.1"
+  VERSION = "0.5.2" # about to release? rerun `bundle lock` to update Gemfile.lock
 end
